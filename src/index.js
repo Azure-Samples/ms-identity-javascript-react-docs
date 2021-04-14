62df46f4978f9e76e00d4c42acc97252717b098f
Dhruv Chand <dhruvchand@live.com>
2021-04-14 17:23:54 -0500

    feat: import and initialize MSAL (Microsoft Authentication Library)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Configuration,  PublicClientApplication } from "@azure/msal-browser";
 
+
+const configuration: Configuration = {
+  auth: {
+      clientId: "034ff080-d093-433d-915c-4f12ae03f100"
+  }
+};
+
+const clientInstance = new PublicClientApplication(configuration);
 
 ReactDOM.render(
   <React.StrictMode>

3a6ac16a1dd9d541d5b14f98991c923b46de3ab1
Dhruv Chand <dhruvchand@live.com>
2021-09-24 11:07:21 -0700

    docrefs: use named tagging

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { MsalProvider, useMsal, useIsAuthenticated } from "@azure/msal-react";
 
 
 
-//<configure_msal> 
+//<msdocs_ref_configure_msal> 
 const configuration: Configuration = {
   auth: {
       clientId: "034ff080-d093-433d-915c-4f12ae03f100"
@@ -17,19 +17,19 @@ const configuration: Configuration = {
 
 
 const clientInstance = new PublicClientApplication(configuration);
-//</configure_msal> 
+//</msdocs_ref_configure_msal> 
 
 
 const PageContent = () => {
-  //<use_msal_context>  
+  //<msdocs_ref_use_msal_context>  
 
   const { instance, accounts } = useMsal();
   
-  //</use_msal_context>  
+  //</msdocs_ref_use_msal_context>  
 
   const {name} = accounts[0]||{};
 
-  //</check_logged_in_state>  
+  //</msdocs_ref_check_logged_in_state>  
   const isAuthenticated = useIsAuthenticated();
 
   return isAuthenticated ? (    
@@ -38,18 +38,18 @@ const PageContent = () => {
     </div>    
     ): 
     (  
-      //<add_login_button>
+      //<msdocs_ref_add_login_button>
 
       <div><button onClick={() => instance.loginPopup()}>Login</button> </div>
 
-      //</add_login_button>
+      //</msdocs_ref_add_login_button>
     );
  
-  //</check_logged_in_state>  
+  //</msdocs_ref_check_logged_in_state>  
 
 }
 
-//<use_msal_provider>
+//<msdocs_ref_use_msal_provider>
 
 ReactDOM.render(
   <React.StrictMode>
@@ -62,4 +62,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//</use_msal_provider>
+//</msdocs_ref_use_msal_provider>
